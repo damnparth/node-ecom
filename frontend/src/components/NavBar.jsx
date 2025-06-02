7943f9d45a3ce9445860ffffbdc8668903a28a5d
Parth <dasparth1544@gmail.com>
2025-06-02 17:54:55 +0530

    routes, figuring out tailwind.

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className='flex items-center justify-between py-5 font-medium'>
+
+        <NavLink to='/' classname="flex flex-col items-center gap-1">
+            <p>home</p>
+            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
+        </NavLink>
+         <NavLink to='/collection' classname="flex flex-col items-center gap-1">
+            <p>collection</p>
+            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
+        </NavLink>
+         <NavLink to='/about' classname="flex flex-col items-center gap-1">
+            <p>about</p>
+            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
+        </NavLink>
+         <NavLink to='/contact' classname="flex flex-col items-center gap-1">
+            <p>contact us</p>
+            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
+        </NavLink>
+        
+        
+        
+        
+
+    </div>
   )
 }
 
